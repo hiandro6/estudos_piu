cc81e21a44a55d5f8f1291f4a25e64cc167aee3f
hiandro6 <hiandroalex6@gmail.com>
2025-05-14 17:45:36 -0300

    testes

--- 01aula/p_aula2/src/Change.jsx
+++ 01aula/p_aula2/src/Change.jsx
@@ -2,12 +2,22 @@ import { useState } from "react";
 
 export default function Change(){
     let [count, setCount] = useState(0)
+    let [count2, setCount2] = useState(0)
+    let [clickNumber, setClickNumber] = useState(1);
     // let counter = 0
+
+    function handleClick() {
+        setCount2(count2 + clickNumber);         // soma o valor atual do clique
+        setClickNumber(clickNumber + 1);         // incrementa para o próximo clique
+    }
     return(
         
         <div>
             <button onClick={() => setCount(count + 1)}> click here</button>
             <h1>{count}</h1>
+
+            <button onClick={handleClick}> click here</button>
+            <h1>{count2}</h1>
         </div>
 
     ); 
