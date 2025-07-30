c5b74c29fcb8d39ad2d9c2bdcfe5ae2947d48ec2
Hiandro Alex Silva de Medeiros <20221101110006@ifrn.local>
2025-07-30 15:58:36 -0300

    revisando

--- 12revisao/src/App.jsx
+++ 12revisao/src/App.jsx
@@ -3,15 +3,16 @@ import './App.css'
 import Changetheme from './components/changetheme'
 import InputColor from './components/InputColor'
 import Login from './components/Login'
+import ListarApi from './components/ListarApi'
 
 function App() {
 
   return (
       <Changetheme>
-        <Login/>
-        <h1>revisao</h1>
-        <InputColor/>
-
+        {/* <Login/> */}
+        {/* <h1>revisao</h1> */}
+        {/* <InputColor/> */}
+        <ListarApi/>
       </Changetheme>
       
   )
---
+++ 12revisao/src/components/ListarApi.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+export default function EffectAPI() {
+    const [albuns, setAlbuns] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [inputdata, setInputdata] = useState("");
+    const [atualizar, setAtualizar] = useState(0)
+    useEffect(() => {
+        const fecthAlbuns = async () => {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+            const data = await response.json();
+            setAlbuns(data);
+        } catch (error) {
+            console.error("Erro ao buscar usuários:", error);
+        } finally {
+            setLoading(false);
+        }
+        }
+
+        fecthAlbuns();
+    }, [atualizar]);
+
+    if (loading) {
+        return <p>Carregando...</p>;
+    }
+
+    const filtrados = albuns.filter(item => item.userId === Number(inputdata) );
+
+    return (
+        <div>
+            <h1>Lista de Usuários</h1>
+            <input 
+                type="number" 
+                placeholder="Digite um ID de 1 a 10"
+                onChange={(e) => setInputdata(e.target.value)}
+            />
+            <ul>
+                {filtrados.map(item =>(
+                <li key={item.id}>{item.title}</li>
+                ))}
+            </ul>
+            <button onClick={() => {setAtualizar(atualizar + 1)}}>acessar api</button>
+        </div>
+    );
+    }
